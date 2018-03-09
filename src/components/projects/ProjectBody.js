e6f5b4d1515ae3584290254cfdea502e3b61714b
Blaine Anderson <BlaineA97@Gmail.com>
2018-03-09 16:50:36 -0700

    added 4 button snippets for future use

--- src/components/projects/ProjectBody.js
+++ src/components/projects/ProjectBody.js
@@ -4,7 +4,14 @@ import './ProjectBody.css';
 class ProjectBody extends Component {
   render() {
     return (
-      <div className="ProjectBody-container">
+      <div id="ProjectBody">
+        <div id="ProjectBody-Navigation">
+          <Navigation />
+        </div>
+
+        <div id="ProjectBody-wrapper">
+          {/* Actual Body of Page */}
+        </div>
       </div>
     );
   }
--- src/components/snippets/Snippets.js
+++ src/components/snippets/Snippets.js
@@ -6,13 +6,37 @@ import Navigation from '../Navigation';
 class Snippets extends Component {
   render() {
     return (
-      <div className="Snippets-wrapper">
-        <Navigation />
+      <div id="Snippets">
+        <div id="Snippets-Navigation">
+          <Navigation />
+        </div>
+        <div id="Snippets-wrapper">
 
-        <div id="Snip-button-01">
-          <Link to="" className="Snip-button">
-            Projects
-          </Link>
+          <div id="Snip-buttons-container" className="Snippet-container">
+            <Link to="" id="Snip-button-01" className="Snip-button">
+              Hover Here!
+            </Link>
+            <Link to="" id="Snip-button-02" className="Snip-button">
+              Hover Here!
+            </Link>
+            <Link to="" id="Snip-button-03" className="Snip-button">
+              Hover Here!
+            </Link>
+            <Link to="" id="Snip-button-04" className="Snip-button">
+              <div id="Snip-button-04-border"></div>
+              <p id="Snip-button-04-text">Hover Here!</p>
+            </Link>
+
+          </div>
+
+
+
+          <div id="Snip-testing-container">
+            <Link to="" id="Snip-testing-button">
+              Test Button
+            </Link>
+          </div>
+
         </div>
       </div>
     );
