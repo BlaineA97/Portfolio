8e8e734b847a234a15fefef4a1d00fa47f2ee0ec
Blaine Anderson <BlaineA97@Gmail.com>
2018-05-15 16:58:38 -0700

    added ProjectList .js .css

--- src/components/projects/ProjectBody.js
+++ src/components/projects/ProjectBody.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './ProjectBody.css';
 import Navigation from '../Navigation';
+import ProjectList from './ProjectList';
 import Footer from '../Footer';
 
 class ProjectBody extends Component {
@@ -10,6 +11,7 @@ class ProjectBody extends Component {
         <div id="ProjectBody-Navigation"><Navigation /></div>
 
         <div id="ProjectBody-wrapper">
+          <ProjectList />
         </div>
 
         <div id="ProjectBody-Footer"><Footer /></div>
---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './ProjectList.css';
+
+class ProjectList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      blank: null
+    }
+  }
+
+  render() {
+    return (
+      <div id="ProjectList">
+
+      </div>
+    );
+  }
+}
+
+export default ProjectList;
