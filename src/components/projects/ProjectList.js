9a249ae488efcee1a3ed675ae24f82456cd16949
Blaine Anderson <BlaineA97@Gmail.com>
2018-05-16 00:39:41 -0700

    Added responsive design to base layout for Projects page

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './ProjectList.css';
+import PortfolioScreenShot from '../../images/PortfolioScreenShot.png'
 
 class ProjectList extends Component {
   constructor(props) {
@@ -12,33 +13,27 @@ class ProjectList extends Component {
   render() {
     return (
       <div id="ProjectList">
-        
+
         <div className="ListProject">
           <div className="ListProjectInformation">
             <p className="ProjectName">My Portfolio</p>
-            <p className="ProjectGithub"><a href="https://github.com/BlaineA97/Portfolio">Project Github</a></p>
+            <p className="ProjectGithub">
+              <a href="https://github.com/BlaineA97/Portfolio">
+                Project Github
+              </a>
+            </p>
             <p className="ProjectUrl">You're Here!</p>
+            <p className="ProjectDescription">
+              A website designed to show you all the wonderful things I spend my free time doing :)
+            </p>
           </div>
           <div className="ListProjectImage">
-            <img src="" alt="IMAGE"/>
+            <img src={PortfolioScreenShot}
+              alt="PortfolioImage"
+              className="ListImages"/>
           </div>
         </div>
 
-        <div className="ListProject">
-          <p className="ProjectName">ProjectName</p>
-          <p className="ProjectGithub"><a href="">Project Github</a></p>
-          <p className="ProjectUrl">Project Url</p>
-        </div>
-        <div className="ListProject">
-          <p className="ProjectName">ProjectName</p>
-          <p className="ProjectGithub"><a href="">Project Github</a></p>
-          <p className="ProjectUrl">Project Url</p>
-        </div>
-        <div className="ListProject">
-          <p className="ProjectName">ProjectName</p>
-          <p className="ProjectGithub"><a href="">Project Github</a></p>
-          <p className="ProjectUrl">Project Url</p>
-        </div>
       </div>
     );
   }
