6227c23d8c5fbac825caa0210d1bca1178998cd1
Blaine Anderson <BlaineA97@Gmail.com>
2018-05-15 17:20:32 -0700

    added basic project layout to ProjectList.js and .css

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -12,7 +12,33 @@ class ProjectList extends Component {
   render() {
     return (
       <div id="ProjectList">
+        
+        <div className="ListProject">
+          <div className="ListProjectInformation">
+            <p className="ProjectName">My Portfolio</p>
+            <p className="ProjectGithub"><a href="https://github.com/BlaineA97/Portfolio">Project Github</a></p>
+            <p className="ProjectUrl">You're Here!</p>
+          </div>
+          <div className="ListProjectImage">
+            <img src="" alt="IMAGE"/>
+          </div>
+        </div>
 
+        <div className="ListProject">
+          <p className="ProjectName">ProjectName</p>
+          <p className="ProjectGithub"><a href="">Project Github</a></p>
+          <p className="ProjectUrl">Project Url</p>
+        </div>
+        <div className="ListProject">
+          <p className="ProjectName">ProjectName</p>
+          <p className="ProjectGithub"><a href="">Project Github</a></p>
+          <p className="ProjectUrl">Project Url</p>
+        </div>
+        <div className="ListProject">
+          <p className="ProjectName">ProjectName</p>
+          <p className="ProjectGithub"><a href="">Project Github</a></p>
+          <p className="ProjectUrl">Project Url</p>
+        </div>
       </div>
     );
   }
