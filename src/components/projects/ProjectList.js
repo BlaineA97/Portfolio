38fd001cf7ab8cb6ae182f8f975cba5518b2a6a5
Blaine Anderson <BlaineA97@Gmail.com>
2018-05-16 03:17:37 -0700

    added additional projects. Added Technologies section into initial project

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -26,6 +26,122 @@ class ProjectList extends Component {
             <p className="ProjectDescription">
               A website designed to show you all the wonderful things I spend my free time doing :)
             </p>
+            <div className="ProjectTechnologies">
+              {/* Insert Tech Classes Later */}
+              <img src={PortfolioScreenShot} alt="TechImage" className="TechImages"/>
+              <img src={PortfolioScreenShot} alt="TechImage" className="TechImages"/>
+              <img src={PortfolioScreenShot} alt="TechImage" className="TechImages"/>
+              <img src={PortfolioScreenShot} alt="TechImage" className="TechImages"/>
+            </div>
+          </div>
+          <div className="ListProjectImage">
+            <img src={PortfolioScreenShot}
+              alt="PortfolioImage"
+              className="ListImages"/>
+          </div>
+        </div>
+
+        <div className="ListProject">
+          <div className="ListProjectInformation">
+            <p className="ProjectName">Dungeons</p>
+            <p className="ProjectGithub">
+              <a href="https://github.com/BlaineA97/Dungeons">
+                Project Github
+              </a>
+            </p>
+            <p className="ProjectUrl">
+              <a href="https://serene-inlet-45614.herokuapp.com/">
+                Visit Project Example
+              </a>
+            </p>
+            <p className="ProjectDescription">
+              A small battle system highlighting ReactJS props and states along with a large amount of Javascript functionality.
+            </p>
+            <p className="ProjectDescription">
+              This project is planned to be expanded.
+            </p>
+          </div>
+          <div className="ListProjectImage">
+            <img src={PortfolioScreenShot}
+              alt="PortfolioImage"
+              className="ListImages"/>
+          </div>
+        </div>
+
+        <div className="ListProject">
+          <div className="ListProjectInformation">
+            <p className="ProjectName">Progressive Power Postal</p>
+            <p className="ProjectGithub">
+              <a href="https://github.com/BlaineA97/ProgressivePowerPostal">
+                Project Github
+              </a>
+            </p>
+            <p className="ProjectUrl">
+              <a href="">
+                Visit Project Example
+              </a>
+            </p>
+            <p className="ProjectDescription">
+              A ReactJS singlepage application reimagining the original website.
+            </p>
+            <p className="ProjectDescription">
+              This project is planned to be expanded.
+            </p>
+          </div>
+          <div className="ListProjectImage">
+            <img src={PortfolioScreenShot}
+              alt="PortfolioImage"
+              className="ListImages"/>
+          </div>
+        </div>
+
+        <div className="ListProject">
+          <div className="ListProjectInformation">
+            <p className="ProjectName">Veteran Affairs Data Display</p>
+            <p className="ProjectGithub">
+              <a href="https://github.com/BlaineA97/VeteransProject">
+                Project Github
+              </a>
+            </p>
+            <p className="ProjectUrl">
+              <a href="">
+                Visit Project Example
+              </a>
+            </p>
+            <p className="ProjectDescription">
+              A ReactJS singlepage application that displays information dynamically.
+            </p>
+            <p className="ProjectDescription">
+              This project is planned to be expanded.
+            </p>
+          </div>
+          <div className="ListProjectImage">
+            <img src={PortfolioScreenShot}
+              alt="PortfolioImage"
+              className="ListImages"/>
+          </div>
+        </div>
+
+        <div className="ListProject">
+          <div className="ListProjectInformation">
+            <p className="ProjectName">Sautee Seattle</p>
+            <p className="ProjectGithub">
+              <a href="https://github.com/Sautee-Seattle/Sautee-Seattle">
+                Project Github
+              </a>
+            </p>
+            <p className="ProjectUrl">
+              <a href="">
+                Visit Project Example
+              </a>
+            </p>
+            <p className="ProjectDescription">
+              A Ruby on Rails Web Application which scrapes  sources to bring dynamic lists of season based produce.
+              Also has user functionality to provide easy local farmers market locations using the application.
+            </p>
+            <p className="ProjectDescription">
+              This project is completed.
+            </p>
           </div>
           <div className="ListProjectImage">
             <img src={PortfolioScreenShot}
